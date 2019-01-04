3474d0e13485d99e2574d0e070e2cdfcc551a4d7
Junya Kobayashi <atsubeee@doi.ics.keio.ac.jp>
2019-01-04 15:06:00 +0900

    animate

--- index.js
+++ index.js
@@ -1,11 +1,60 @@
 $(function () {
-  $('#fullpage').fullpage();
+  // var fullpageEvent = function (index, nextIndex, direction) {
+  //   scrollingSpeed: 200
+  // }
 
-  const anim = lottie.loadAnimation({
-    container: document.getElementById('bodymovin-wrapper'),
-    render: 'svg',
-    loop: true,
-    path: 'loading.json'
+  $('.activity').on('inview', function () {
+    $('.activity .container').delay(1000).queue(function () {
+      $(this).fadeIn().dequeue();
+    });
+  });
+
+  $('.interview').on('inview', function () {
+    $('.interview .container').delay(1000).queue(function () {
+      $(this).fadeIn().dequeue();
+    });
+  });
+
+  $('.story').on('inview', function () {
+    $('.story .container').delay(1000).queue(function () {
+      $(this).fadeIn().dequeue();
+    });
+    // $('.story .container').fadeIn();
+  });
+
+  $('.schedule').on('inview', function () {
+    $('.schedule .container').delay(1000).queue(function () {
+      $(this).fadeIn().dequeue();
+    });
+  });
+
+  $('.production').on('inview', function () {
+    $('.production .container').delay(1000).queue(function () {
+      $(this).fadeIn().dequeue();
+    });
+  });
+
+  $('.future-vision').on('inview', function () {
+    $('.future-vision .container').delay(1000).queue(function () {
+      $(this).fadeIn().dequeue();
+    });
   })
-  anim.play();
+
+  $('#fullpage').fullpage({
+    normalScrollElementTouchThreshold: 10,
+    touchSensitivity: 10,
+    fadingEffect: true,
+    fitToSectionDelay: 2000
+    // scrollingSpeed: 2000
+  });
+
+  // console.log()
+
+  // const anim = lottie.loadAnimation({
+  //   container: document.getElementById('bodymovin-wrapper'),
+  //   render: 'svg',
+  //   loop: true,
+  //   path: 'loading.json'
+  // })
+  // anim.play();
 });
